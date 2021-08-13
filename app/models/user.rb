9ed593b05f6db3c6516b7d3790a15216055dc9dd
LazyCoderC <396666417@qq.com>
2021-08-13 19:40:52 +0800

    add 0 and 1 verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }
   validates :password, presence: true, length: { maximum: 255 }
   validates :name, presence: true, length: { maximum: 255 }
+  validates :status, format: { with: /^[0|1]*$/ , message: "only allows letters",:multiline => true}
 end
