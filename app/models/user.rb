beb4ad91e57255d00889048f1c53456949860def
LazyCoderC <396666417@qq.com>
2021-08-13 20:14:33 +0800

    change the status to active/inactive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  enum status: { active: 1, inactive: 0}
+  enum status: { active: 1, inactive: 0 }
+
+
   validates :email, presence: true, length: { maximum: 255 }
   validates :password, presence: true, length: { maximum: 255 }
   validates :name, presence: true, length: { maximum: 255 }
-  validates :status, format: { with: /^[0|1]*$/ , message: "only allows letters",:multiline => true}
+  validates :status, inclusion: { in: %w(active inactive) }
 end
