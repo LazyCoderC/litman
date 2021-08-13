9130ec885cec7dfb6423096ec984ca9b40d0f37f
LazyCoderC <396666417@qq.com>
2021-08-13 20:55:46 +0800

    add article curd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_110152) do
+ActiveRecord::Schema.define(version: 2021_08_13_125435) do
+
+  create_table "articles", charset: "utf8mb4", force: :cascade do |t|
+    t.string "user_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email"
@@ -21,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_08_13_110152) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email"
     t.index ["status"], name: "index_users_on_status"
   end
