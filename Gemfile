a5f9168dc20fef506b217bf876e89b5dbcfdf0a8
LazyCoderC <396666417@qq.com>
2021-08-14 17:52:30 +0800

    post to requestBin

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 
 gem 'httparty', '~> 0.18.1'
+
+gem 'open-uri'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,15 +1,33 @@
 class Animal < ApplicationRecord
   include HTTParty
 
+
+
   def self.fetch
-    url = 'https://axoltlapi.herokuapp.com/'
-    response = HTTParty.get(url)
-    ceshi = JSON.parse(response.to_json)
-    url = ceshi["url"]
-    facts = ceshi["facts"]
-    pics_repo = ceshi["pics_repo"]
-    api_repo = ceshi["api_repo"]
+    base_uri = 'https://axoltlapi.herokuapp.com/'
+    response = HTTParty.get(base_uri)
+    api = JSON.parse(response.to_json)
+    url = api["url"]
+    facts = api["facts"]
+    pics_repo = api["pics_repo"]
+    api_repo = api["api_repo"]
     Animal.create(url: url, facts: facts, pics_repo: pics_repo, api_repo: api_repo)
     # raise api_repo.to_json
+  end
+
+  def push_remote
+    animal = Animal.first
+    # raise animal.to_json
+    HTTParty.post("http://requestbin.net/r/f1c4d0z1", {
+      body: {
+        url: animal.url,
+        facts: animal.facts,
+        pics_repo: animal.pics_repo,
+        api_repo: animal.api_repo
+      },
+      headers: {
+        'Content-Type' => 'application/x-www-form-urlencoded',
+        'charset' => 'utf-8'}
+    })
   end
 end
