334a6fe895a11f997b425ecf8fea34257f9284cc
LazyCoderC <396666417@qq.com>
2021-08-14 17:03:49 +0800

    add parsing api to animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,15 @@
 class Animal < ApplicationRecord
+  include HTTParty
+
+  def self.fetch
+    url = 'https://axoltlapi.herokuapp.com/'
+    response = HTTParty.get(url)
+    ceshi = JSON.parse(response.to_json)
+    url = ceshi["url"]
+    facts = ceshi["facts"]
+    pics_repo = ceshi["pics_repo"]
+    api_repo = ceshi["api_repo"]
+    Animal.create(url: url, facts: facts, pics_repo: pics_repo, api_repo: api_repo)
+    # raise api_repo.to_json
+  end
 end
